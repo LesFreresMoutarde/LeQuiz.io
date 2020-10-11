fabc9642cb0cf22dc764b5e544284d2b370c7b2e
EmileCalixte <calixte.emile@gmail.com>
2020-10-11 20:59:47 +0200

    Header top right content

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,22 @@
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCog} from "@fortawesome/free-solid-svg-icons";
+import {Link} from "react-router-dom";
 
 class Header extends React.Component {
     render = () => {
         return (
             <header className="app-header">
-                Header
+                <div className="header-user">
+                    <div className="header-user-name">
+                        Current user
+                    </div>
+                    <div className="header-user-settings-cog">
+                        <Link to="/settings">
+                            <FontAwesomeIcon icon={faCog} />
+                        </Link>
+                    </div>
+                </div>
             </header>
         )
     }
