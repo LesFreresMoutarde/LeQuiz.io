b8575a09b54cd3a9f9a3224bdb18290946823224
EmileCalixte <calixte.emile@gmail.com>
2020-10-11 20:11:13 +0200

    Exported footer in a separated React component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,25 +5,19 @@ import {Switch, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import CreateRoom from "./pages/CreateRoom";
 import JoinRoom from "./pages/JoinRoom";
+import Footer from "./Footer";
 
 function App() {
     return (
         <div className="app">
             <div className="content-wrapper">
-                <p>Toujours là</p>
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/create-room" component={CreateRoom} />
                     <Route exact path="/join-room" component={JoinRoom} />
                 </Switch>
             </div>
-            <footer>
-                <div className="footer-text">
-                    <span className="footer-text-item">© LeQuiz.io {(new Date()).getFullYear()}</span>
-                    -
-                    <span className="footer-text-item"><a href="#" target="_blank">Mentions légales</a></span>
-                </div>
-            </footer>
+            <Footer />
         </div>
     );
 }
