586d877840214e746ad193d29687a9af96305598
Alexis <alexis.lejeune@gmx.fr>
2020-10-12 18:54:58 +0200

    Switching Deleting Mode Of User Model, Physical to Logical

--- backend/models/dbModels/user.js
+++ backend/models/dbModels/user.js
@@ -74,6 +74,7 @@ module.exports = (sequelize, DataTypes) => {
         sequelize,
         modelName: 'User',
         tableName: 'user',
+        paranoid: true,
         indexes: [{
             fields: ['username', 'email', 'plan']
         }]
