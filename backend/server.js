9d49762e6913e2f434b0897931432ba8af016c72
Alexis <alexis.lejeune@gmx.fr>
2020-10-22 11:17:34 +0200

    Create Model User Const

--- backend/server.js
+++ backend/server.js
@@ -195,6 +195,7 @@ testModel = async () => {
         });
         res.map((result) => console.log(result.toJSON()));
         console.log("la souscription de l'utilisateur", await user.getSubscriptions());
+        console.log("les plans possibles", db.User.userPlans);
     } catch (error) {
         // Rollback si erreur
         await transaction.rollback();
