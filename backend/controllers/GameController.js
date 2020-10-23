7146bc450fa1d7d6a05e9f27b74cbff6990a328d
aeki <aekiwow@hotmail.fr>
2020-10-23 11:36:23 +0200

    generate code room

--- backend/controllers/GameController.js
+++ backend/controllers/GameController.js
@@ -2,6 +2,21 @@ const MainController = require('./mainController/MainController');
 
 class GameController extends MainController {
 
+    generateCodeRoom = () => {
+        let codeRoom = "";
+        const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        //tableau à remplir des codes room déja réservé par d'autre rooms
+        let arrayCodeRoom = [];
+
+        while (arrayCodeRoom.includes(codeRoom) || codeRoom === "") {
+            codeRoom = "";
+            for (var i = 0; i < 6; i++) {
+                codeRoom += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+        }
+
+        return codeRoom;
+    }
 }
 
-module.exports = MainController;
+module.exports = GameController;
