e2ca70e276aafee21ae0244baf3e03edd1b5a217
Emile Calixte <calixte.emile@gmail.com>
2020-12-30 22:01:25 +0100

    Added sendgrid dependency

--- backend/util/Util.js
+++ backend/util/Util.js
@@ -1,4 +1,8 @@
 const argon2 = require('argon2');
+const sendgrid = require('@sendgrid/mail');
+const env = require('../config/env');
+
+sendgrid.setApiKey(env.email.apiKey);
 
 class Util {
     static Password = {
@@ -11,6 +15,24 @@ class Util {
             });
         }
     };
+
+    static Email = {
+        FROM_NAME: 'LeQuiz.io',
+        FROM_NOREPLY_ADDRESS: 'noreply@lequiz.io',
+
+        sendEmail: async(message = {}) => {
+            await sendgrid.send(message);
+        },
+
+        sendEmailFromNoreply: async(message = {}) => {
+            message.from = {
+                email: Util.Email.FROM_NOREPLY_ADDRESS,
+                name: Util.Email.FROM_NAME,
+            }
+
+            Util.Email.sendEmail(message);
+        },
+    }
 }
 
 module.exports = Util;
