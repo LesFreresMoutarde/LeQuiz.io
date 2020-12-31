a0392176201b8ce810e5c8f6931ac3a1d1e87e99
aeki <aekiwow@hotmail.fr>
2020-12-31 17:59:47 +0100

    home style

--- frontend/src/components/pages/Home/Home.js
+++ frontend/src/components/pages/Home/Home.js
@@ -5,11 +5,22 @@ class Home extends React.Component {
     render = () => {
         return (
             <>
-                <ul>
-                    <li><Link to="/create-room">Create room</Link></li>
-                    <li><Link to="/join-room">Join room</Link></li>
-                </ul>
+                <div className="label-title">LE QUIZ
+                </div>
+                <div className="home-menu">
+                    <div className="home-menu-wrapper">
+                        <button className="btn-create-room btn-menu" onClick={() => document.location.href = "/create-room"}>
+                            <p className="home-menu-label">Créer un Salon</p>
+                        </button>
+                    </div>
+                    <div className="home-menu-wrapper">
+                        <button className="btn-join-room btn-menu" onClick={() => document.location.href = "/join-room"}>
+                            <p className="home-menu-label">Rejoindre un salon</p>
+                        </button>
+                    </div>
+                </div>
             </>
+
         );
     }
 }
