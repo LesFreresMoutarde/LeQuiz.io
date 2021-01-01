daea02439c85c96a234081d3b49fc05d5050a59a
Alexis <alexis.lejeune@gmx.fr>
2021-01-01 12:40:32 +0100

    Create Question Header component

---
+++ frontend/src/components/pages/Room/components/Question/QuestionHeader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const QuestionHeader = ({round, category, type, timeLeft}) => {
+   return (
+       <div className="question-header">
+
+       </div>
+   )
+};
+
+export default QuestionHeader;
--- frontend/src/components/pages/Room/views/Question.js
+++ frontend/src/components/pages/Room/views/Question.js
@@ -2,6 +2,7 @@ import React from "react";
 import Title from "../../../misc/Title";
 import QcmAnswer from "../components/Question/QcmAnswer";
 import LeaveRoomCross from "../components/Shared/LeaveRoomCross";
+import QuestionHeader from "../components/Question/QuestionHeader";
 
 
 class Question extends React.Component {
@@ -18,15 +19,21 @@ class Question extends React.Component {
             case 'qcm':
                 return (
                     <>
-                        <LeaveRoomCross leaveRoom={leaveRoom}/>
-                        <Title title={`Question ${round}`}/>
-                        <p>Temps restant : {timeLeft}</p>
-                        <p>{category}</p>
+                        <QuestionHeader round={round} category={category} type={type} timeLeft={timeLeft}/>
+                        <div>
+                            <p>Temps restant : {timeLeft}</p>
+                            <Title title={`Question ${round}`}/>
+                            <div> <!-- Flex direction: colum -->
+                                <p>{category}</p>
+                                <p>{type}</p>
+                            </div>
+                        </div>
                         <p>{content}</p>
                         {answer.answers.map((answer, index) => (
                                 <QcmAnswer key={index} answer={answer} disabled={questionInputDisabled} submitAnswer={submitAnswer}/>
                             )
                         )}
+                        <LeaveRoomCross leaveRoom={leaveRoom}/>
                     </>);
             case 'input':
         }
