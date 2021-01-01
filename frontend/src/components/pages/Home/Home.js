789ebc176c04fe9e75bcb6565f4af36e31d4f47f
aeki <aekiwow@hotmail.fr>
2021-01-01 16:49:17 +0100

    style home

--- frontend/src/components/pages/Home/Home.js
+++ frontend/src/components/pages/Home/Home.js
@@ -1,21 +1,32 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import Title from "../../../components/misc/Title"
 
 class Home extends React.Component {
+
+    static TITLE = 'LE QUIZ';
+
+    createRoom = () => {
+        this.props.history.replace('/create-room');
+    };
+
+    joinRoom = () => {
+        this.props.history.replace('/join-room');
+    };
+
     render = () => {
         return (
             <>
-                <div className="label-title">LE QUIZ
-                </div>
+                <Title title={Home.TITLE}/>
                 <div className="home-menu">
                     <div className="home-menu-wrapper">
-                        <button className="btn-create-room btn-menu" onClick={() => document.location.href = "/create-room"}>
-                            <p className="home-menu-label">Créer un Salon</p>
+                        <button className="home-menu-create home-menu-btn" onClick={this.createRoom}>
+                            <p className="home-menu-btn-label">Créer un Salon</p>
                         </button>
                     </div>
                     <div className="home-menu-wrapper">
-                        <button className="btn-join-room btn-menu" onClick={() => document.location.href = "/join-room"}>
-                            <p className="home-menu-label">Rejoindre un salon</p>
+                        <button className="home-menu-join home-menu-btn" onClick={this.joinRoom}>
+                            <p className="home-menu-btn-label">Rejoindre un salon</p>
                         </button>
                     </div>
                 </div>
