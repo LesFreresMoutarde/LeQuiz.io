f61ea7478adbef4a87b59be3c4ca25a84544f8d3
Alexis <alexis.lejeune@gmx.fr>
2021-01-01 17:02:25 +0100

    Change Design Of Game Mode Due To Better Understanding Of Flexbox

--- frontend/src/components/pages/CreateGame/components/GameMode.js
+++ frontend/src/components/pages/CreateGame/components/GameMode.js
@@ -3,12 +3,10 @@ import React from "react";
 const GameMode = ({gameMode, pickGameMode}) => {
     const {label, description, allowed, classname} = gameMode;
     return (
-        <div className="game-mode-wrapper">
         <button className={`game-mode gm-${classname.toLowerCase()}`} onClick={() => pickGameMode(gameMode)} disabled={!allowed}>
             <p className="game-mode-label">{label}</p>
             <p className="game-mode-description">{description}</p>
         </button>
-        </div>
     );
 };
 
