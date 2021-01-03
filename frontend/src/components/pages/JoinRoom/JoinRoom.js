eb8fec651964fcc1badaf8e77456393522c09fd0
aeki <aekiwow@hotmail.fr>
2021-01-03 13:45:01 +0100

    join room input

--- frontend/src/components/pages/JoinRoom/JoinRoom.js
+++ frontend/src/components/pages/JoinRoom/JoinRoom.js
@@ -1,12 +1,34 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import Title from "../../misc/Title";
+import BackArrow from "../../misc/BackArrow";
+import {placeholder} from "@babel/types";
 
 class JoinRoom extends React.Component {
+
+    static TITLE = 'LE QUIZ';
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true
+        };
+    }
+
+    goBack = () => {
+        this.props.history.goBack();
+    }
+
     render = () => {
         return (
             <>
-                <Link to="/">Back home</Link>
-                <div>Join room</div>
+            <Title title={JoinRoom.TITLE}/>
+            <BackArrow onClick={this.goBack}/>
+            <div className="join-room-div">
+                <input type="text" id="join-room-input" placeholder="Code de la partie"/>
+            <br/>
+                <button className="join-room-enter">Entrer</button>
+            </div>
             </>
         );
     }
