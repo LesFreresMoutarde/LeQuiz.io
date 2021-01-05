4eaaeb3bed73d44b13b74a0557fdbf363ee0334a
Emile Calixte <calixte.emile@gmail.com>
2021-01-05 15:28:40 +0100

    Removed legal notice

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,8 +6,8 @@ class Footer extends React.Component {
             <footer>
                 <div className="footer-text">
                     <span className="footer-text-item">© LeQuiz.io {(new Date()).getFullYear()}</span>
-                    -
-                    <span className="footer-text-item"><a href="#" target="_blank">Mentions légales</a></span>
+                    {/*-*/}
+                    {/*<span className="footer-text-item"><a href="#" target="_blank">Mentions légales</a></span>*/}
                 </div>
             </footer>
         )
