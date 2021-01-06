e9d8ddfc908ac3c4fb04cce350aafab267022463
aeki <aekiwow@hotmail.fr>
2021-01-06 09:25:06 +0100

    return

--- frontend/src/components/pages/Login/Login.js
+++ frontend/src/components/pages/Login/Login.js
@@ -4,6 +4,7 @@ import Util from "../../../util/Util";
 import App from "../../App";
 
 import Toastr from "toastr2";
+import BackArrow from "../../misc/BackArrow";
 const toastr = new Toastr();
 
 class Login extends React.Component {
@@ -17,6 +18,10 @@ class Login extends React.Component {
         Util.UserAccess.componentRequiresRole(Util.UserAccess.ROLES.GUEST_ONLY);
     }
 
+    goBack = () => {
+        this.props.history.goBack();
+    };
+
     render = () => {
         if(this.state.redirect) {
             return(
@@ -25,26 +30,29 @@ class Login extends React.Component {
         }
 
         return(
-            <div className="text-center">
-                <h1 className="mb">Connexion</h1>
-                <p className="mb2">Tu n'as pas encore de compte ? <Link to="/register">Inscription</Link></p>
-                <form id="login-form" onSubmit={this.onLoginFormSubmit}>
-                    <div className="mb3 mt3">
-                        <input className="full-width" id="username-input" name="username" placeholder="Nom d'utilisateur ou adresse email" autoFocus autoComplete="username" required/>
-                    </div>
-                    <div className="mb3">
-                        <input className="full-width" id="password-input" type="password" name="password" placeholder="Mot de passe" autoComplete="current-password" required/>
-                    </div>
-                    <div className="mb3 text-left">
-                        <label className="checkbox">
-                            <input type="checkbox" id="stay-logged-in-checkbox" name="stayLoggedIn"/>
-                            <span>Rester connecté</span>
-                        </label>
-                    </div>
-                    <button type="submit" className="button green mb2">Connexion</button>
-                </form>
-                <small><Link to="/forgot-password">Mot de passe oublié</Link></small>
-            </div>
+            <>
+                <BackArrow onClick={this.goBack}/>
+                <div className="text-center">
+                    <h1 className="mb">Connexion</h1>
+                    <p className="mb2">Tu n'as pas encore de compte ? <Link to="/register">Inscription</Link></p>
+                    <form id="login-form" onSubmit={this.onLoginFormSubmit}>
+                        <div className="mb3 mt3">
+                            <input className="full-width" id="username-input" name="username" placeholder="Nom d'utilisateur ou adresse email" autoFocus autoComplete="username" required/>
+                        </div>
+                        <div className="mb3">
+                            <input className="full-width" id="password-input" type="password" name="password" placeholder="Mot de passe" autoComplete="current-password" required/>
+                        </div>
+                        <div className="mb3 text-left">
+                            <label className="checkbox">
+                                <input type="checkbox" id="stay-logged-in-checkbox" name="stayLoggedIn"/>
+                                <span>Rester connecté</span>
+                            </label>
+                        </div>
+                        <button type="submit" className="button green mb2">Connexion</button>
+                    </form>
+                    <small><Link to="/forgot-password">Mot de passe oublié</Link></small>
+                </div>
+            </>
         )
     }
 
