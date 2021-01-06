d42da9a38ce68c149f11e769d5bda4f7efee5e51
Emile Calixte <calixte.emile@gmail.com>
2021-01-06 20:20:58 +0100

    Set JWT secret key in backend env file

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -10,8 +10,7 @@ class AuthController extends MainController {
     static TOKEN_TYPE_ACCESS_TOKEN = 'accessToken';
     static TOKEN_TYPE_REFRESH_TOKEN = 'refreshToken';
 
-    // TODO from a config (env...)
-    static JWT_SECRET = 'superSecret';
+    static JWT_SECRET = env.jwtSecret;
     static ACCESS_TOKEN_LIFETIME = 60 * 15; // 15 minutes
     static REFRESH_TOKEN_LIFETIME = 60 * 60 * 24; // 1 day
     static REFRESH_TOKEN_LIFETIME_STAY_LOGGED_IN = 60 * 60 * 24 * 365 // 1 year
