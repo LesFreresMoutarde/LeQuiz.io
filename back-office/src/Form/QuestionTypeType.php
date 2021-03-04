88dfdffe7e1377655f71a0a2e666a4ceaf516f5a
alexisljn <alexis.lejeune@gmx.fr>
2021-03-04 21:15:42 +0100

    Add Question Type Crud

---
+++ back-office/src/Form/QuestionTypeType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\QuestionType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QuestionTypeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('label')
+            ->add('ischild')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => QuestionType::class,
+        ]);
+    }
+}
