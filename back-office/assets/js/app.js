479ce767fa86c82b9c6a036d0ccca3298ef9ed33
aeki <aekiwow@hotmail.fr>
2021-03-31 21:44:21 +0200

    sidebar

--- back-office/assets/js/app.js
+++ back-office/assets/js/app.js
@@ -7,8 +7,24 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../styles/app.scss';
+import '../styles/nav.scss';
 
 import { Tooltip, Toast, Popover } from 'bootstrap';
 
 // start the Stimulus application
 import './bootstrap';
+
+alert('good');
+
+(function () {
+    isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
+
+    if (isWindows) {
+        // if we are on windows OS we activate the perfectScrollbar function
+        $('.sidebar .sidebar-wrapper, .main-panel').perfectScrollbar();
+
+        $('html').addClass('perfect-scrollbar-on');
+    } else {
+        $('html').addClass('perfect-scrollbar-off');
+    }
+})();
--- back-office/webpack.config.js
+++ back-office/webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
     .addEntry('app', './assets/js/app.js')
     .addEntry('login', './assets/js/login.js')
+    .addEntry('home', './assets/js/home.js')
 
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
