1960e8579ee7231d76c13a8274ed936a6fb5d086
aeki <aekiwow@hotmail.fr>
2021-04-06 02:29:35 +0200

    adjustment

--- back-office/assets/js/app.js
+++ back-office/assets/js/app.js
@@ -1,30 +1,12 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
-import '../styles/app.scss';
 import '../styles/nav.scss';
+import '../styles/app.scss';
 
-import { Tooltip, Toast, Popover } from 'bootstrap';
+import $ from 'jquery'
 
-// start the Stimulus application
-import './bootstrap';
+$('.sidebar').mouseover(function (){
+    $('#content-body').css({"width" : "calc(100% - 260px)"})
+});
 
-alert('good');
-
-(function () {
-    isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
-
-    if (isWindows) {
-        // if we are on windows OS we activate the perfectScrollbar function
-        $('.sidebar .sidebar-wrapper, .main-panel').perfectScrollbar();
-
-        $('html').addClass('perfect-scrollbar-on');
-    } else {
-        $('html').addClass('perfect-scrollbar-off');
-    }
-})();
+$('.sidebar').mouseleave(function (){
+    $('#content-body').css({"width" : "calc(100% - 100px)"})
+});
---
+++ back-office/assets/js/home.js
@@ -0,0 +1,10 @@
+import '../styles/home.scss';
+
+import $ from 'jquery'
+
+
+$( document ).ready(function() {
+    $('#sidebar-home').addClass('active');
+});
+
+
