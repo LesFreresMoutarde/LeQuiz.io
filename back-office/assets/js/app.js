da1671455e177611c1e90c40ab290ca9925372fa
aeki <aekiwow@hotmail.fr>
2021-04-24 18:08:36 +0200

    responsive pages and nav horizontal on small screen

--- back-office/assets/js/app.js
+++ back-office/assets/js/app.js
@@ -3,10 +3,13 @@ import '../styles/app.scss';
 
 import $ from 'jquery'
 
-$('.sidebar').mouseover(function () {
-    $('#content-body').css({"width": "calc(100% - 260px)"})
-});
+$('.icon-menu').click(function () {
+    $('.small-menu').toggle();
+})
 
-$('.sidebar').mouseleave(function () {
-    $('#content-body').css({"width": "calc(100% - 100px)"})
+$('body').click(function (event)
+{
+    if(!$(event.target).closest('.sidebar').length && !$(event.target).is('.sidebar')) {
+        $(".small-menu").hide();
+    }
 });
