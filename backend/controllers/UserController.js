22b9bdcfe617347c0992a632f2cd9f667955a4d1
alexisljn <alexis.lejeune@gmx.fr>
2021-04-25 23:44:39 +0200

    PR Advices

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -6,16 +6,16 @@ class UserController extends MainController {
     actionContact = async ({username, email, subject, message}) => {
         await EmailUtil.sendEmailFromNoreply({
             to: EmailUtil.CONTACT_ADDRESS,
-            subject: `${username} a envoyé une demande : ${subject}`,
+            subject: `${username} a envoyé un message  : ${subject}`,
             replyTo: email,
             html:
-`<p style="font-size: 1.4em; margin-bottom: 20px">${username} a envoyé une demande : ${subject}</p>
+`<p style="font-size: 1.4em; margin-bottom: 20px">${username} a envoyé un message : ${subject}</p>
 <p style="margin-bottom: 30px">${message}</p>
 <p>${username}</p>
 <p>${email}</p>
 `,
             text:
-`${username} a envoyé une demande : ${subject}.
+`${username} a envoyé un message : ${subject}.
 ${message}.
 ${username}
 ${email}
