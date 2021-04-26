1db9c59349f665fb0841496c81069e7c5674892e
alexisljn <alexis.lejeune@gmx.fr>
2021-04-26 19:05:38 +0200

    Modal waiting for backend implementation

--- frontend/src/components/pages/FeedbackModal/FeedbackModal.js
+++ frontend/src/components/pages/FeedbackModal/FeedbackModal.js
@@ -1,6 +1,30 @@
 import React, {useState} from "react";
 import Toastr from "toastr2";
 
+
+const overlayStyle = {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    zIndex: 1,
+    padding: 30,
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end'
+};
+
+const modalStyle = {
+    position: 'absolute',
+    height: 500,
+    width: 450,
+    backgroundColor: '#1e8eb5',
+    zIndex: 2,
+    border: '4px white solid',
+    borderRadius: 10,
+    padding: 30,
+}
+
 const FeedbackModal = ({closeModal}) => {
 
     const toastr = new Toastr()
@@ -12,30 +36,44 @@ const FeedbackModal = ({closeModal}) => {
         try {
             // Verifier que message ne soit pas vide
             if (message === '')
-                throw new Error('message ne peut pas être vide');
+                throw new Error('message vide');
+
+
+
         } catch (error) {
             toastr.error(error.message);
         }
     }
 
+    const handleKeyPress = (charCode) => {
+        if (charCode === 13) sendFeedback()
+    }
+
     return (
         <div onClick={() => closeModal()}
-             style={{position: 'absolute', height: "100%", width: '100%', backgroundColor: 'rgba(0,0,0,0.5)', zIndex: 1}}
+             style={overlayStyle}
         >
-            <div style={{position: 'absolute', height: 500, width: 600,
-                backgroundColor: '#1e8eb5', zIndex: 2, border: '4px white solid', borderRadius: 10,
-                padding: 30}}
+            <div style={modalStyle}
                  onClick={(e) => {e.stopPropagation()}}
             >
-                <p style={{fontSize: '1.4em', textAlign: "center", marginBottom: 20}}>Feedback</p>
+                <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start'}}>
+                    <p style={{fontSize: '1.4em', marginBottom: 20}}>Feedback</p>
+                    <button type="button" onClick={() => closeModal()}>X</button>
+                </div>
+
                 <div style={{marginBottom: 20}}>
                     <input type="text"
+                           style={{width:'80%'}}
                            value={subject}
                            placeholder="sujet"
-                           onChange={(e) => setSubject(e.target.value)}/>
+                           autoFocus={true}
+                           onChange={(e) => setSubject(e.target.value)}
+                           onKeyPress={(e) => handleKeyPress(e.charCode)}
+                    />
                 </div>
                 <div style={{marginBottom: 20}}>
                     <textarea value={message}
+                              style={{width:'80%'}}
                               onChange={(e) => setMessage(e.target.value)}
                               placeholder="message"
                               cols="30"
