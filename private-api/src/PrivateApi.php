4fcba8fa33fba403db5553f61d8de57724190a7b
Emile Calixte <calixte.emile@gmail.com>
2021-04-29 22:55:28 +0200

    Private API initialization

---
+++ private-api/src/PrivateApi.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace PrivateApi;
+
+
+class PrivateApi
+{
+
+}
---
+++ private-api/www/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require '../vendor/autoload.php';
+
+dump(new \PrivateApi\PrivateApi());
+
+echo "Hello world";
