e76c5e17bd3f95bad094e1d0a225f92a8b6ca6f2
Emile Calixte <calixte.emile@gmail.com>
2021-05-09 10:23:53 +0200

    Test template rendering in HelloResponder

--- private-api/src/Responder/HelloResponder.php
+++ private-api/src/Responder/HelloResponder.php
@@ -4,6 +4,7 @@
 namespace PrivateApi\Responder;
 
 
+use PrivateApi\Misc\Util;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -11,7 +12,13 @@ class HelloResponder implements ResponderInterface
 {
     public function respond (ServerRequestInterface $request, ResponseInterface $response, $args)
     {
-        $response->getBody()->write("Hello world!");
+//        $response->getBody()->write("Hello world!");
+//        return $response;
+
+        $response->getBody()->write(Util::renderTemplate(__DIR__ . '/../Email/Template/reset-password.text-plain.php', [
+            'username' => 'Toto',
+            'resetPasswordUrl' => 'http://example.com/reset',
+        ]));
         return $response;
     }
 }
