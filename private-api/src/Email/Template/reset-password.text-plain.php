0e911040eb398d829b5808c3b93dc1cbb881a853
Emile Calixte <calixte.emile@gmail.com>
2021-05-09 17:17:18 +0200

    Update reset-password text/plain template

--- private-api/src/Email/Template/reset-password.text-plain.php
+++ private-api/src/Email/Template/reset-password.text-plain.php
@@ -5,11 +5,11 @@
 
 ?>
 
-Bonjour <?= $username ?>, <?= PHP_EOL ?><?= PHP_EOL ?>
-
-Pour réinitialiser votre mot de passe leQuiz.io, merci d'utiliser le lien ci-dessous. <?= PHP_EOL ?><?= PHP_EOL ?>
-
-<?= $resetPasswordUrl ?> <?= PHP_EOL ?><?= PHP_EOL ?>
-
+Bonjour <?= $username ?>, <?= PHP_EOL ?>
+<?= PHP_EOL ?>
+Pour réinitialiser votre mot de passe leQuiz.io, merci d'utiliser le lien ci-dessous. <?= PHP_EOL ?>
+<?= PHP_EOL ?>
+<?= $resetPasswordUrl ?> <?= PHP_EOL ?>
+<?= PHP_EOL ?>
 Si vous n'êtes pas à l'origine de la demande de réinitialisation de mot de passe, veuillez ignorer cet
 email.
