d1f258ed369236ea81aad065643ce874e97d7eb8
Emile Calixte <calixte.emile@gmail.com>
2021-05-18 14:17:09 +0200

    dump&die uncatched exceptions only in dev mode

--- private-api/www/index.php
+++ private-api/www/index.php
@@ -7,6 +7,7 @@ require '../vendor/autoload.php';
 try { // TODO find a better way to catch and display all errors in dev mode ?
     (new PrivateApi())->run();
 } catch (Throwable $e) {
-    // TODO only in dev mode
-    dd($e);
+    if (PrivateApi::$static->getConfig()->isDevMode()) {
+        dd($e);
+    }
 }
