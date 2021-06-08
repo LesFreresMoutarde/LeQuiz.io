fc65628d387a4c55ad5c4d6a812a6f07396da868
Emile Calixte <calixte.emile@gmail.com>
2021-06-08 22:06:46 +0200

    Add no-underline links

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,8 +6,8 @@ const Footer = ({displayFeedbackModal}) => {
         <footer>
             <div className="footer-text">
                 <span className="footer-text-item">© LeQuiz.io {(new Date()).getFullYear()}</span>
-                <span className="footer-text-item"><a href="/contact">Contact</a></span>
-                <span className="footer-text-item"><a href="/mentions-legales" target="_blank">Mentions légales</a></span>
+                <span className="footer-text-item"><a href="/contact" className="no-underline">Contact</a></span>
+                <span className="footer-text-item"><a href="/mentions-legales" className="no-underline" target="_blank">Mentions légales</a></span>
                 <button className="footer-text-item" onClick={() => displayFeedbackModal()}>Feedback</button>
             </div>
         </footer>
