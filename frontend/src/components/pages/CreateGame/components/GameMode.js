38a78d206cf859cb33f7ca9e2963662d83177dfd
Emile Calixte <calixte.emile@gmail.com>
2021-06-14 16:40:56 +0200

    Split game mode CSS files

--- frontend/src/components/pages/CreateGame/components/GameMode.js
+++ frontend/src/components/pages/CreateGame/components/GameMode.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import '../../../../css/gameMode.css';
 
 const GameMode = ({gameMode, pickGameMode}) => {
     const {label, description, allowed, classname} = gameMode;
