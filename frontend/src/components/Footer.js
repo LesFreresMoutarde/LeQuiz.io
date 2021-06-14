2ef5a49357de165d42b15403d1d3ca49275e8006
Emile Calixte <calixte.emile@gmail.com>
2021-06-14 09:44:21 +0200

    Style footer feedback button

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,9 +6,9 @@ const Footer = ({displayFeedbackModal}) => {
         <footer>
             <div className="footer-text">
                 <span className="footer-text-item">© LeQuiz.io {(new Date()).getFullYear()}</span>
-                <span className="footer-text-item"><a href="/contact" className="no-underline">Contact</a></span>
                 <span className="footer-text-item"><a href="/mentions-legales" className="no-underline" target="_blank">Mentions légales</a></span>
-                <button className="footer-text-item" onClick={() => displayFeedbackModal()}>Feedback</button>
+                <span className="footer-text-item"><a href="/contact" className="no-underline">Contact</a></span>
+                <button className="footer-text-item footer-feedback-button" onClick={() => displayFeedbackModal()}>Feedback</button>
             </div>
         </footer>
     );
