889fc51db13f8458b61f34fb9b797e827caba1b2
Emile Calixte <calixte.emile@gmail.com>
2021-06-15 10:05:19 +0200

    First gamemodes responsive layout

--- frontend/src/components/pages/CreateGame/components/GameMode.js
+++ frontend/src/components/pages/CreateGame/components/GameMode.js
@@ -20,22 +20,31 @@ const GameMode = ({gameMode, pickGameMode}) => {
         }, 200); // CSS animation duration is 0.2s
     }
 
+    const getGameModeLabelContent = () => {
+        return (
+            <>
+                {label}
+                {(() => {
+                    if (!allowed) {
+                        return(
+                            <img className="game-mode-label-disabled" src="/img/icons/locked.svg" />
+                        );
+                    }
+                })()}
+            </>
+        )
+    }
+
     return (
         <button className={`game-mode gm-${classname.toLowerCase()} ${!allowed ? 'disabled' : ''} ${playDisabledAnimation ? 'disabled-animation' : ''}`} onClick={onClick}>
+            <div className="game-mode-label-mobile">{getGameModeLabelContent()}</div>
             <div className="game-mode-button-content">
                 <div className="game-mode-label-container">
                     <div className="game-mode-icon-container">
                         <img className="game-mode-icon" src={`/img/icons/gamemodes/${classname.toLowerCase()}.svg`} alt={`Logo ${label}`}/>
                     </div>
-                    <span className="game-mode-label">
-                        {label}
-                        {(() => {
-                            if (!allowed) {
-                                return(
-                                    <img className="game-mode-label-disabled" src="/img/icons/locked.svg" />
-                                );
-                            }
-                        })()}
+                    <span className="game-mode-label-desktop">
+                        {getGameModeLabelContent()}
                     </span>
                 </div>
                 <div className={"game-mode-description-container"}>
