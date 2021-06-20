6d4ff06de99b23616052238d9110dab1a40f8232
alexisljn <alexis.lejeune@gmx.fr>
2021-06-20 08:15:46 +0200

    Remove unbanDate from Symfony Form; handled directly in front

--- back-office/src/Form/EditUserType.php
+++ back-office/src/Form/EditUserType.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -35,7 +34,6 @@ class EditUserType extends AbstractType
             ->add('isTrustyWriter')
             ->add('isActive')
             ->add('isBanned')
-//            ->add('unbanDate', TextType::class)
             ->add('submit', SubmitType::class, ['label' => 'Modifier'])
         ;
     }
