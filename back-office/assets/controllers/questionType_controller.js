f0ec67b838dc3b0ae9b2d602283cbfa696d417ee
alexisljn <alexis.lejeune@gmx.fr>
2021-06-25 22:41:48 +0200

    Finish Index View

--- back-office/assets/controllers/questionType_controller.js
+++ back-office/assets/controllers/questionType_controller.js
@@ -3,9 +3,34 @@ import Util from "./util_controller";
 
 export default class extends Controller {
 
-    ENTITY_NAME = 'questions';
-    BLOCK_TO_REPLACE = '#questions-block';
+    ENTITY_NAME = 'question-types';
+    BLOCK_TO_REPLACE = '#questionTypes-block';
     page;
     timer;
+
+    filters = {
+        search: '',
+        uuid: '',
+        isChild: false,
+    }
+
+    initialize = () => {
+        this.page = parseInt(Util.getQueryStringParam('page', 1, 'number'));
+
+        const paramsInUrl = Util.getQueryStringParams();
+        Util.fillFilterInputs(paramsInUrl, this.filters);
+    }
+
+    onInput = (evt) => {
+        Util.onFormInput(this.timer, this.filters, this.ENTITY_NAME, this.BLOCK_TO_REPLACE, evt);
+    }
+
+    onUniqueCheckboxChange = async (evt) => {
+        await Util.onUniqueCheckboxChange(this.filters, this.ENTITY_NAME, this.BLOCK_TO_REPLACE, evt);
+    }
+
+    //TODO IN V2 : Un type de question est-il supprimable ?
+    deleteQuestionType = async (evt) => {
+    }
 
 }
