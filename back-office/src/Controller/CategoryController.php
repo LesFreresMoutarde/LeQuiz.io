8cd5aacd48226507a932f526684d807cd3ae91a0
alexisljn <alexis.lejeune@gmx.fr>
2021-06-26 10:27:50 +0200

    Change Index Method To Filter Categories based on params

--- back-office/src/Controller/CategoryController.php
+++ back-office/src/Controller/CategoryController.php
@@ -5,19 +5,49 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use App\Util\Util;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 #[Route('/categories')]
 class CategoryController extends AbstractController
 {
+    private const POSSIBLE_FILTERS = ['search', 'uuid'];
+
     #[Route('/', name: 'category_index', methods: ['GET'])]
-    public function index(CategoryRepository $categoryRepository): Response
+    public function index(Request $request,
+                          EntityManagerInterface $em,
+                          PaginatorInterface $paginator,
+                          Environment $environment): Response
     {
+        $page = 0 !== $request->query->getInt('page') ? $request->query->getInt('page') : 1;
+
+        $params = Util::getParamFromUrl($request, self::POSSIBLE_FILTERS);
+
+        $categories = $this->getFilteredCategories($page, $params, $em, $paginator);
+
+        if ($request->headers->has('X-Requested-With')) {
+
+            $response = new Response();
+
+            $template = $environment->load('category/index.html.twig');
+
+            $response->headers->set('Content-Type', 'text/plain');
+
+            $response->setStatusCode(Response::HTTP_OK);
+
+            $response->setContent($template->renderBlock('categories', ['categories' => $categories]));
+
+            $response->send();
+        }
+
         return $this->render('category/index.html.twig', [
-            'categories' => $categoryRepository->findAll(),
+            'categories' => $categories,
         ]);
     }
 
@@ -78,5 +108,39 @@ class CategoryController extends AbstractController
         }
 
         return $this->redirectToRoute('category_index');
+    }
+
+    private function getFilteredCategories(int $page, array $params, EntityManagerInterface $em, PaginatorInterface $paginator)
+    {
+        $queryString = 'SELECT c from App\Entity\Category c';
+        $whereParts = [];
+        $paramsReplacements = [];
+
+        if (!empty($params)) {
+            foreach ($params as $paramName => $value) {
+                switch ($paramName) {
+                    case 'search':
+                        $whereParts[] = '(LOWER(c.name) LIKE LOWER(:search) OR LOWER(c.label) LIKE LOWER(:search))';
+                        $paramsReplacements['search'] = '%'.$value.'%';
+                        break;
+                    case 'uuid':
+                        if (Util::isUuidValid($value)) {
+                            $whereParts[] = 'c.id = :uuid';
+                            $paramsReplacements['uuid'] = $value;
+                        }
+                        break;
+                }
+            }
+        }
+
+        $whereString = implode(' AND ', $whereParts);
+        $queryString .= !empty($whereString) > 0
+            ? ' WHERE '.$whereString
+            : '';
+
+        $query = $em->createQuery($queryString);
+        $query->setParameters($paramsReplacements);
+
+        return $paginator->paginate($query, $page, 10);
     }
 }
