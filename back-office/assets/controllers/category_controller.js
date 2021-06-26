b6365e1f97e4a925d715f1b1742fbaeba6c08328
alexisljn <alexis.lejeune@gmx.fr>
2021-06-26 11:54:21 +0200

    Complete Category Controller

--- back-office/assets/controllers/category_controller.js
+++ back-office/assets/controllers/category_controller.js
@@ -11,7 +11,23 @@ export default class extends Controller {
     filters = {
         search: '',
         uuid: '',
-        isChild: false,
+    }
+
+    initialize = () => {
+        this.page = parseInt(Util.getQueryStringParam('page', 1, 'number'));
+
+        const paramsInUrl = Util.getQueryStringParams();
+        Util.fillFilterInputs(paramsInUrl, this.filters);
+    }
+
+    onInput = (evt) => {
+        Util.onFormInput(this.timer, this.filters, this.ENTITY_NAME, this.BLOCK_TO_REPLACE, evt);
+    }
+
+    //TODO IN V2 : What Happens when category has questions related to itself
+    deleteCategory = async (evt) => {
+        const uuid = evt.currentTarget.getAttribute('data-uuid');
+        await Util.deleteEntity(this.ENTITY_NAME, uuid);
     }
 
 }
