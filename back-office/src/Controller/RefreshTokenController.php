d84b07f60359c3b06a83089ecf067fba04c6c05f
alexisljn <alexis.lejeune@gmx.fr>
2021-06-27 23:15:51 +0200

    Rewrite index method

--- back-office/src/Controller/RefreshTokenController.php
+++ back-office/src/Controller/RefreshTokenController.php
@@ -4,20 +4,53 @@ namespace App\Controller;
 
 use App\Entity\RefreshToken;
 use App\Form\RefreshTokenType;
+use App\Manager\CrudManager;
 use App\Repository\RefreshTokenRepository;
+use App\Util\Util;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 #[Route('/refresh-tokens')]
 class RefreshTokenController extends AbstractController
 {
+    private const POSSIBLE_FILTERS = ['search'];
+
     #[Route('/', name: 'refresh_token_index', methods: ['GET'])]
-    public function index(RefreshTokenRepository $refreshTokenRepository): Response
+    public function index(
+        Request $request,
+        EntityManagerInterface $em,
+        PaginatorInterface $paginator,
+        Environment $environment,
+    ): Response
     {
+        $page = 0 !== $request->query->getInt('page') ? $request->query->getInt('page') : 1;
+
+        $params = Util::getParamFromUrl($request, self::POSSIBLE_FILTERS);
+
+        $refreshTokens = $this->getFilteredRefreshTokens($page, $params, $em, $paginator);
+
+        if ($request->headers->has('X-Requested-With')) {
+
+            $response = new Response();
+
+            $template = $environment->load('refresh_token/index.html.twig');
+
+            $response->headers->set('Content-Type', 'text/plain');
+
+            $response->setStatusCode(Response::HTTP_OK);
+
+            $response->setContent($template->renderBlock('refreshTokens', ['refreshTokens' => $refreshTokens]));
+
+            $response->send();
+        }
+
         return $this->render('refresh_token/index.html.twig', [
-            'refresh_tokens' => $refreshTokenRepository->findAll(),
+            'refreshTokens' => $refreshTokens,
         ]);
     }
 
@@ -39,5 +72,32 @@ class RefreshTokenController extends AbstractController
         }
 
         return $this->redirectToRoute('refresh_token_index');
+    }
+
+    private function getFilteredRefreshTokens(int $page, array $params, EntityManagerInterface $em, PaginatorInterface $paginator)
+    {
+        $queryString = 'SELECT r from App\Entity\RefreshToken r';
+        $joinParts = [];
+        $paramsReplacements = [];
+
+        if (!empty($params)) {
+            foreach ($params as $paramName => $value) {
+                switch ($paramName) {
+                    case 'search':
+                        $joinParts[] = 'JOIN r.user u WITH u.username LIKE LOWER(:search)';
+                        $paramsReplacements['search'] = '%'.$value.'%';
+                        break;
+                }
+            }
+        }
+
+        $joinString = implode(' ', $joinParts);
+        $queryString .= !empty($joinString) > 0
+            ? ' '.$joinString
+            : '';
+
+        $query = $em->createQuery($queryString);
+        $query->setParameters($paramsReplacements);
+        return $paginator->paginate($query, $page, 10);
     }
 }
