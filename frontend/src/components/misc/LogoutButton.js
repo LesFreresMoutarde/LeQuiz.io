9272cd4eddb38a90231fe54157d9400d3a2178d0
Emile Calixte <calixte.emile@gmail.com>
2021-06-29 12:27:36 +0200

    Finished header items alignment (desktop only)

--- frontend/src/components/misc/LogoutButton.js
+++ frontend/src/components/misc/LogoutButton.js
@@ -30,7 +30,7 @@ class LogoutButton extends React.Component {
 
             return (
                 <svg className="logout-button" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1"
-                     x="0px" y="0px" viewBox="8 0 100 100" height="36" width="36" fill="white"
+                     x="0px" y="0px" viewBox="8 19 100 64" height="26" width="36" fill="white"
                      xmlSpace="preserve">
                     <path className="logout-button-arrow" strokeWidth="3" stroke="white"
                           d="M90.4,51c0-1.1-0.4-2.1-1.2-2.8L74.4,33.3c-1.6-1.6-4.1-1.6-5.7,0c-1.6,1.6-1.6,4.1,0,5.7l8,8H37   c-2.2,0-4,1.8-4,4s1.8,4,4,4h39.8l-8,8c-1.6,1.6-1.6,4.1,0,5.7c0.8,0.8,1.8,1.2,2.8,1.2s2-0.4,2.8-1.2l14.8-14.8   C90,53.1,90.4,52.1,90.4,51z"/>
