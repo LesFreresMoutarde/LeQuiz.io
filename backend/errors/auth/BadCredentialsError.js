4dbfb1f040bcd6fc71f968318dbc28b0d700127a
alexisljn <alexis.lejeune@gmx.fr>
2021-06-30 22:47:38 +0200

    Create base error : Forbidden; create child errors : BadCredentials, BannedUser, NotLoggedUser

--- backend/errors/auth/BadCredentialsError.js
+++ backend/errors/auth/BadCredentialsError.js
@@ -2,8 +2,8 @@ const NotFoundError = require("../base/NotFoundError");
 
 class BadCredentialsError extends NotFoundError {
 
-    constructor() {
-        super('Les informations saisies ne correspondent à aucun utilisateur');
+    constructor(message = 'Les informations saisies ne correspondent à aucun utilisateur') {
+        super(message);
     }
 
 }
