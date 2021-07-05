162de18f8c261644882fd32db6d3baa33cbc8799
alexisljn <alexis.lejeune@gmx.fr>
2021-07-05 22:37:11 +0200

    Add Submit Button to Form

--- back-office/src/Form/TagType.php
+++ back-office/src/Form/TagType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Tag;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +15,7 @@ class TagType extends AbstractType
         $builder
             ->add('name')
             ->add('label')
-            ->add('createdAt')
-            ->add('updatedAt')
+            ->add('submit', SubmitType::class)
         ;
     }
 
