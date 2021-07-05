adc8bf0eaf44c4ed76b9bab020274598bf5370da
alexisljn <alexis.lejeune@gmx.fr>
2021-07-05 22:09:14 +0200

    Implement Index Tags

--- back-office/src/Controller/TagController.php
+++ back-office/src/Controller/TagController.php
@@ -5,19 +5,51 @@ namespace App\Controller;
 use App\Entity\Tag;
 use App\Form\TagType;
 use App\Repository\TagRepository;
+use App\Util\Util;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 #[Route('/tags')]
 class TagController extends AbstractController
 {
+    private const POSSIBLE_FILTERS = ['search', 'uuid'];
+
     #[Route('/', name: 'tag_index', methods: ['GET'])]
-    public function index(TagRepository $tagRepository): Response
-    {
+    public function index
+    (
+        Request $request,
+        EntityManagerInterface $em,
+        PaginatorInterface $paginator,
+        Environment $environment
+    ): Response {
+        $page = 0 !== $request->query->getInt('page') ? $request->query->getInt('page') : 1;
+
+        $params = Util::getParamFromUrl($request, self::POSSIBLE_FILTERS);
+
+        $tags = $this->getFilteredTags($page, $params, $em, $paginator);
+
+        if ($request->headers->has('X-Requested-With')) {
+
+            $response = new Response();
+
+            $template = $environment->load('tag/index.html.twig');
+
+            $response->headers->set('Content-Type', 'text/plain');
+
+            $response->setStatusCode(Response::HTTP_OK);
+
+            $response->setContent($template->renderBlock('tags', ['tags' => $tags]));
+
+            $response->send();
+        }
+
         return $this->render('tag/index.html.twig', [
-            'tags' => $tagRepository->findAll(),
+            'tags' => $tags,
         ]);
     }
 
@@ -79,4 +111,39 @@ class TagController extends AbstractController
 
         return $this->redirectToRoute('tag_index');
     }
+
+    private function getFilteredTags(int $page, array $params, EntityManagerInterface $em, PaginatorInterface $paginator)
+    {
+        $queryString = 'SELECT tg from App\Entity\Tag tg';
+        $whereParts = [];
+        $paramsReplacements = [];
+
+        if (!empty($params)) {
+            foreach ($params as $paramName => $value) {
+                switch ($paramName) {
+                    case 'search':
+                        $whereParts[] = '(LOWER(tg.name) LIKE LOWER(:search) OR LOWER(tg.label) LIKE LOWER(:search))';
+                        $paramsReplacements['search'] = '%'.$value.'%';
+                        break;
+                    case 'uuid':
+                        if (Util::isUuidValid($value)) {
+                            $whereParts[] = 'tg.id = :uuid';
+                            $paramsReplacements['uuid'] = $value;
+                        }
+                        break;
+                }
+            }
+        }
+
+        $whereString = implode(' AND ', $whereParts);
+        $queryString .= !empty($whereString) > 0
+            ? ' WHERE '.$whereString
+            : '';
+
+        $query = $em->createQuery($queryString);
+        $query->setParameters($paramsReplacements);
+
+        return $paginator->paginate($query, $page, 10);
+    }
+
 }
