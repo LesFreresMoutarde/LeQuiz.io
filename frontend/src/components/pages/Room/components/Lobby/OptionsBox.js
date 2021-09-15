5e722cc1d52ffa1446c8f63dc69edcaecb9c5260
Emile Calixte <calixte.emile@gmail.com>
2021-09-15 21:48:44 +0200

    Finish style of lobby options box

--- frontend/src/components/pages/Room/components/Lobby/OptionsBox.js
+++ frontend/src/components/pages/Room/components/Lobby/OptionsBox.js
@@ -13,7 +13,7 @@ const OptionsBox = ({questionTypes, winCriterion, withHardcoreQuestions, changeO
             <div className="lobby-box-content">
                 <section>
                     <div className="lobby-options-label">Types de questions</div>
-                    <div className="lobby-value-container">
+                    <div className="lobby-options-question-types-container">
                         {questionTypes.map((questionType, index) => (
                             <div className="lobby-options-question-type" key={index}>
                                 {questionType.label}
@@ -25,14 +25,16 @@ const OptionsBox = ({questionTypes, winCriterion, withHardcoreQuestions, changeO
 
                 <section>
                     <div className="lobby-options-label">Questions "hardcore"</div>
-                    <div className="lobby-value-container">
-                        {withHardcoreQuestions &&
+                    <div className="lobby-options-with-hardcore-questions-value-container">
+                        <div className="lobby-options-with-hardcore-questions-value">
+                            {withHardcoreQuestions &&
                             "Avec"
-                        }
+                            }
 
-                        {!withHardcoreQuestions &&
+                            {!withHardcoreQuestions &&
                             "Sans"
-                        }
+                            }
+                        </div>
                     </div>
                 </section>
 
