c2ca6015b1fa48f2d472c96ab5cd39e9b6393a0d
Emile Calixte <calixte.emile@gmail.com>
2021-09-23 21:42:16 +0200

    Style question content

--- frontend/src/components/pages/Room/components/Question/ClassicQuestion.js
+++ frontend/src/components/pages/Room/components/Question/ClassicQuestion.js
@@ -1,9 +1,24 @@
 import React from "react";
 
 const ClassicQuestion = ({question, submitAnswer, userCanSubmit, isQcmEnabled, enableQcm}) => {
+    console.log(question);
+
     return (
         <div className="classic-question-container">
-            Classic question
+            <div className="question-content-container">
+                <div className="question-content">
+                    {question.content}
+                </div>
+                <div className="question-counter">
+                    Question X sur Y
+                </div>
+            </div>
+
+            <div className="question-answers-container">
+                Answers
+            </div>
+
+
         </div>
     )
 }
