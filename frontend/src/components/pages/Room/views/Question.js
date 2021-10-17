914ac7b3b84588d9d951ea10f492567473a6e66d
Emile Calixte <calixte.emile@gmail.com>
2021-10-17 18:06:16 +0200

    Clean Question component file

--- frontend/src/components/pages/Room/views/Question.js
+++ frontend/src/components/pages/Room/views/Question.js
@@ -5,8 +5,6 @@ import '../../../../css/pages/question.css';
 import QuitCross from "../../../misc/QuitCross";
 
 const Question = ({currentQuestion, quizLength, submitAnswer, timeLeft, questionInputDisabled, leaveRoom, isQcmEnabled, enableQcm}) => {
-    console.log(currentQuestion)
-
     return (
         <div className="question-screen-container">
             <div className="question-screen-desktop-header">
@@ -50,83 +48,5 @@ const Question = ({currentQuestion, quizLength, submitAnswer, timeLeft, question
         </div>
     );
 }
-
-
-// class Question extends React.Component {
-//
-//     constructor(props) {
-//         super(props);
-//         this.qcmAnswersColors = Util.getRandomColors(4);
-//     }
-//
-//     render() {
-//         const {content, type, round, category, answer, typeLabel} = this.props.currentQuestion;
-//         const { submitAnswer, timeLeft, questionInputDisabled, leaveRoom, isQcmEnabled, enableQcm } = this.props;
-//
-//         switch (type) {
-//             case 'classic':
-//             default:
-//                 return (
-//                     isQcmEnabled
-//                     ?
-//                         <div className="question-screen-container">
-//
-//                             <div className="question-screen-left">
-//                                 <Clock timeLeft={timeLeft}/>
-//                                 <LeaveRoomCross leaveRoom={leaveRoom}/>
-//                             </div>
-//
-//                             <div className="question-screen-center">
-//                                 <p className="question-round">{`Question ${round}`}</p>
-//                                 <p className="question-content mb3">{content}</p>
-//                                 <div className="qcm-container">
-//                                     {answer.answers.qcm.map((answer, index) => (
-//                                         <QcmPick key={index}
-//                                                  colorClass={this.qcmAnswersColors[index]}
-//                                                  answer={answer}
-//                                                  disabled={questionInputDisabled}
-//                                                  submitAnswer={submitAnswer}/>
-//                                         )
-//                                     )}
-//                                 </div>
-//                             </div>
-//                             <div className="question-screen-right">
-//                                 <div className="question-header-info">
-//                                     <p className={`question-header-info-box ${this.qcmAnswersColors[0]}`}>{category}</p>
-//                                     <p className={`question-header-info-box ${this.qcmAnswersColors[1]}`}>{typeLabel}</p>
-//                                 </div>
-//                             </div>
-//                         </div>
-//
-//                     :
-//
-//                         <div className="question-screen-container">
-//
-//                             <div className="question-screen-left">
-//                                 <Clock timeLeft={timeLeft}/>
-//                                 <LeaveRoomCross leaveRoom={leaveRoom}/>
-//                             </div>
-//
-//                             <div className="question-screen-center">
-//                                 <p className="question-round">{`Question ${round}`}</p>
-//                                 <p className="question-content mb3">{content}</p>
-//                                 <div>
-//                                     <InputValue submitAnswer={submitAnswer}/>
-//                                 </div>
-//                             </div>
-//                             <button onClick={enableQcm}>Activer QCM</button>
-//                             <div className="question-screen-right">
-//                                 <div className="question-header-info">
-//                                     <p className={`question-header-info-box ${this.qcmAnswersColors[0]}`}>{category}</p>
-//                                     <p className={`question-header-info-box ${this.qcmAnswersColors[1]}`}>{typeLabel}</p>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                 );
-//         }
-//     }
-//
-//
-// }
 
 export default Question;
