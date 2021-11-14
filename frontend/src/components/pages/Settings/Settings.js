a70b80f929ce8d0af8ee43a7e4c933d439a4fff5
Emile Calixte <calixte.emile@gmail.com>
2021-11-14 22:03:08 +0100

    Prepare new Settings view

--- frontend/src/components/pages/Settings/Settings.js
+++ frontend/src/components/pages/Settings/Settings.js
@@ -1,103 +1,134 @@
-import React from "react";
+import React, {useEffect} from "react";
 import EmailSettings from "./views/EmailSettings";
 import PasswordSettings from "./views/PasswordSettings";
 import {ON_CLICK_GO_BACK} from "../../misc/BackArrow";
 import UserAccessUtil from "../../../util/UserAccessUtil";
 import {app} from "../../App";
 
-class Settings extends React.Component {
-    constructor(props) {
-        super(props);
+const CONTENT_EMAIL = "email";
+const CONTENT_PASSWORD = "password";
 
-        UserAccessUtil.componentRequiresRole(UserAccessUtil.ROLES.LOGGED_IN);
+const Settings = () => {
 
-        this.state = {
-            desktopCurrentContent: "email",
-        }
-    }
-
-    componentDidMount() {
-        this.showDesktopSection('email');
+    useEffect(() => {
         app.showBackArrow(true, ON_CLICK_GO_BACK);
-    }
+    }, []);
 
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        UserAccessUtil.componentRequiresRole(UserAccessUtil.ROLES.LOGGED_IN); // TODO find a better/more generic solution ?
-
-        return true;
-    }
-
-    onNavigate = (e) => {
-        const li = e.target.parentNode;
-        const navTo = li.getAttribute('data-desktop-nav');
-        if (navTo === null) {
-            return;
-        }
-
-        this.showDesktopSection(navTo);
-        this.setLateralMenuActiveItem(navTo);
-    }
-
-    showDesktopSection(name) {
-        const sectionId = `desktop-content-${name}`;
-
-        const sectionsContainer = document.getElementById('desktop-variable-content');
-
-        for (const section of sectionsContainer.childNodes) {
-            if(section.id === sectionId) {
-                section.classList.add('active');
-                continue;
-            }
-
-            section.classList.remove('active');
-        }
-    }
-
-    setLateralMenuActiveItem(name) {
-        const nav = document.getElementById('page-lateral-navigation');
-        const ul = nav.childNodes[0];
-
-        for(const li of ul.childNodes) {
-            if(li.getAttribute('data-desktop-nav') === name) {
-                li.classList.add('active');
-                continue;
-            }
-
-            li.classList.remove('active');
-        }
-    }
-
-    goBack = () => {
-        this.props.history.goBack();
-    };
-
-    render = () => {
-        return (
-            <div className="flex-container">
-                <nav id="page-lateral-navigation">
-                    <ul>
-                        <li data-desktop-nav="email" className="active"><a onClick={this.onNavigate}>Adresse email</a></li>
-                        <li data-desktop-nav="password"><a onClick={this.onNavigate}>Mot de passe</a></li>
-                    </ul>
-                </nav>
-                <div className="flex-item-full-space">
-                    <h1 className="mb">Paramètres</h1>
-
-                    <div id="desktop-variable-content">
-
-                        <section id="desktop-content-email">
-                            <EmailSettings/>
-                        </section>
-
-                        <section id="desktop-content-password">
-                            <PasswordSettings/>
-                        </section>
-
+    return (
+        <div className="settings-container">
+            <div className="settings-title">
+                <h1>Paramètres</h1>
+            </div>
+            <div className="settings-mobile-scrollable-container">
+                <div className="settings-data-container">
+                    <aside className="settings-desktop-menu-container">
+                        Lateral menu
+                    </aside>
+                    <div className="settings-main-data">
+                        Settings
                     </div>
+                    <aside className="settings-desktop-menu-counterbalance">
+
+                    </aside>
                 </div>
             </div>
-        )
-    }
+        </div>
+    );
 }
+
+// class Settings extends React.Component {
+//     constructor(props) {
+//         super(props);
+//
+//         UserAccessUtil.componentRequiresRole(UserAccessUtil.ROLES.LOGGED_IN);
+//
+//         this.state = {
+//             desktopCurrentContent: "email",
+//         }
+//     }
+//
+//     componentDidMount() {
+//         this.showDesktopSection('email');
+//         app.showBackArrow(true, ON_CLICK_GO_BACK);
+//     }
+//
+//     shouldComponentUpdate(nextProps, nextState, nextContext) {
+//         UserAccessUtil.componentRequiresRole(UserAccessUtil.ROLES.LOGGED_IN); // TODO find a better/more generic solution ?
+//
+//         return true;
+//     }
+//
+//     onNavigate = (e) => {
+//         const li = e.target.parentNode;
+//         const navTo = li.getAttribute('data-desktop-nav');
+//         if (navTo === null) {
+//             return;
+//         }
+//
+//         this.showDesktopSection(navTo);
+//         this.setLateralMenuActiveItem(navTo);
+//     }
+//
+//     showDesktopSection(name) {
+//         const sectionId = `desktop-content-${name}`;
+//
+//         const sectionsContainer = document.getElementById('desktop-variable-content');
+//
+//         for (const section of sectionsContainer.childNodes) {
+//             if(section.id === sectionId) {
+//                 section.classList.add('active');
+//                 continue;
+//             }
+//
+//             section.classList.remove('active');
+//         }
+//     }
+//
+//     setLateralMenuActiveItem(name) {
+//         const nav = document.getElementById('page-lateral-navigation');
+//         const ul = nav.childNodes[0];
+//
+//         for(const li of ul.childNodes) {
+//             if(li.getAttribute('data-desktop-nav') === name) {
+//                 li.classList.add('active');
+//                 continue;
+//             }
+//
+//             li.classList.remove('active');
+//         }
+//     }
+//
+//     goBack = () => {
+//         this.props.history.goBack();
+//     };
+//
+//     render = () => {
+//         return (
+//             <div className="flex-container">
+//                 <nav id="page-lateral-navigation">
+//                     <ul>
+//                         <li data-desktop-nav="email" className="active"><a onClick={this.onNavigate}>Adresse email</a></li>
+//                         <li data-desktop-nav="password"><a onClick={this.onNavigate}>Mot de passe</a></li>
+//                     </ul>
+//                 </nav>
+//                 <div className="flex-item-full-space">
+//                     <h1 className="mb">Paramètres</h1>
+//
+//                     <div id="desktop-variable-content">
+//
+//                         <section id="desktop-content-email">
+//                             <EmailSettings/>
+//                         </section>
+//
+//                         <section id="desktop-content-password">
+//                             <PasswordSettings/>
+//                         </section>
+//
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 export default Settings;
