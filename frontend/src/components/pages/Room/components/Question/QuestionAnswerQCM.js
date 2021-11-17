6e65d609fb47fde4a5901fbcc57f4cfbcc20bda5
alexisljn <alexis.lejeune@gmx.fr>
2021-11-17 21:58:40 +0100

    Create method that submit answer and set in its state the picked answer

--- frontend/src/components/pages/Room/components/Question/QuestionAnswerQCM.js
+++ frontend/src/components/pages/Room/components/Question/QuestionAnswerQCM.js
@@ -5,6 +5,12 @@ import Util from "../../../../../util/Util";
 const QuestionAnswerQCM = ({question, userCanSubmit, submitAnswer}) => {
     const [colorIndexes, setColorIndexes] = useState(Util.randomizeArray([1, 2, 3, 4]));
     const [answers, setAnswers] = useState(Util.randomizeArray([...question.answer.answers.qcm]));
+    const [pickedAnswer, setPickedAnswer] = useState(null);
+
+    const submitAnswerAndDisableInputs = (answer) => {
+        setPickedAnswer(answer);
+        submitAnswer(answer);
+    }
 
     return (
         <div className="question-answer-qcm-container">
@@ -12,8 +18,9 @@ const QuestionAnswerQCM = ({question, userCanSubmit, submitAnswer}) => {
                 return (
                     <QcmPick key={index}
                              answer={answer}
+                             pickedAnswer={pickedAnswer}
                              disabled={!userCanSubmit}
-                             submitAnswer={submitAnswer}
+                             submitAnswer={submitAnswerAndDisableInputs}
                              colorIndex={colorIndexes[index]}
                     />
                 )
