a18a25bdb187d917af618248286ec1e4569f37bc
alexisljn <alexis.lejeune@gmx.fr>
2021-11-17 21:57:57 +0100

    create method to get get disable class for qcm answer if not selected by player

--- frontend/src/components/pages/Room/components/Question/QcmPick.js
+++ frontend/src/components/pages/Room/components/Question/QcmPick.js
@@ -1,10 +1,21 @@
 import React from "react";
 
-const QcmPick = ({answer, submitAnswer, disabled, colorIndex}) => {
+const QcmPick = ({answer, submitAnswer, disabled, colorIndex, pickedAnswer}) => {
+
+    const getClasses = () => {
+        if (pickedAnswer !== null && pickedAnswer.content !== answer.content) {
+
+            return `question-answer-qcm-answer qcm-answer-color-${colorIndex} qcm-answer-disabled`;
+        }
+
+        return `question-answer-qcm-answer qcm-answer-color-${colorIndex}`;
+    }
+
+    const classes = getClasses();
 
     return (
         <div className="question-answer-qcm-answer-container">
-            <button className={`question-answer-qcm-answer qcm-answer-color-${colorIndex}`}
+            <button className={classes}
                     disabled={disabled}
                     onClick={() => submitAnswer(answer)}
             >
