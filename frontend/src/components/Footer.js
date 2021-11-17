f7bfc39b2f7585882019cb49d48ce1cc258f6261
alexisljn <alexis.lejeune@gmx.fr>
2021-11-17 16:32:50 +0100

    Mobile layout for footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,8 +1,20 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-const Footer = ({displayFeedbackModal}) => {
+const Footer = ({displayFeedbackModal, isMobile}) => {
 
+    if (isMobile) {
+        return (
+            <footer style={{display: 'flex', flexDirection: 'column'}}>
+                <span className="footer-text-item mb">© LeQuiz.io {(new Date()).getFullYear()}</span>
+                <span className="mb">
+                    <span className="footer-text-item"><a href="/mentions-legales" className="no-underline" target="_blank">Mentions légales</a></span>
+                <span className="footer-text-item"><Link to="/contact" className="no-underline">Contact</Link></span>
+                </span>
+                <button className="footer-text-item footer-feedback-button" onClick={() => displayFeedbackModal()}>Feedback</button>
+            </footer>
+        )
+    }
     return (
         <footer>
             <span className="footer-text-item">© LeQuiz.io {(new Date()).getFullYear()}</span>
