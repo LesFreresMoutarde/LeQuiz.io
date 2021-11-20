287ccd538906204a574f037a23646fc101912854
alexisljn <alexis.lejeune@gmx.fr>
2021-11-20 19:53:27 +0100

    Complete class AudioPlayer

--- frontend/src/manager/AudioPlayer.js
+++ frontend/src/manager/AudioPlayer.js
@@ -1,32 +1,90 @@
 class AudioPlayer {
-    static player;
+
+    static players = {};
 
     static sounds = {
-        badAnswer: 'sounds/bad_answer.wav',
-        endGame: 'sounds/end_game.wav',
-        enterRoom: 'sounds/enter_room.wav',
-        goodAnswer: 'sounds/good_answer.wav',
-        leaveRoom: 'sounds/leave_room.wav',
-        sendAnswer: 'sounds/send_answer.wav',
-        startGame: 'sounds/start_game.wav',
-        timer: 'sounds/timer.wav',
+        badAnswer: {
+            path: '/sounds/bad_answer.wav'
+        },
+        endGame: {
+            path: '/sounds/end_game.wav'
+        },
+        enterRoom: {
+            path: '/sounds/enter_room.wav'
+        },
+        goodAnswer: {
+            path: '/sounds/good_answer.wav'
+        },
+        leaveRoom: {
+            path: '/sounds/leave_room.wav'
+        },
+        sendAnswer: {
+            path: '/sounds/send_answer.wav',
+            interruptedBy: ['goodAnswer', 'badAnswer', 'endGame']
+        },
+        startGame: {
+            path: '/sounds/start_game.wav'
+        },
+        timer: {
+            path: '/sounds/timer.wav',
+            interruptedBy: ['goodAnswer', 'badAnswer', 'endGame']
+        },
     };
 
     static initiatePlayer = () => {
-        AudioPlayer.player = new Audio();
+        const playerName = `player-${Date.now()}`
+
+        AudioPlayer.players[playerName] = new Audio();
+
+        return playerName;
     }
 
-   static playSound = (soundName) => {
-       AudioPlayer.player.src = AudioPlayer.sounds[soundName];
-       (async () => {
-           await AudioPlayer.player.play();
-       })()
-   }
+    static playSound = (soundName) => {
+        const playerName = AudioPlayer.getPlayer(soundName);
 
-   static killPlayer = () => {
-       AudioPlayer.player.pause();
-       AudioPlayer.player = null;
-   }
+        AudioPlayer.players[playerName].src = AudioPlayer.sounds[soundName].path;
+
+        AudioPlayer.players[playerName].soundName = soundName;
+
+        AudioPlayer.players[playerName].play()
+            .then(() => {
+                AudioPlayer.players[playerName].addEventListener('pause', () => {
+                    delete AudioPlayer.players[playerName];
+                });
+
+                AudioPlayer.players[playerName].addEventListener('abort', () => {
+                    delete AudioPlayer.players[playerName];
+                });
+            })
+            .catch(() => {
+                delete AudioPlayer.players[playerName];
+            })
+        ;
+    }
+
+    static getPlayer = (soundName) => {
+        const players = Object.entries(AudioPlayer.players);
+
+        // No players currently in use
+        if (players.length === 0) return AudioPlayer.initiatePlayer();
+
+        players.forEach(([playerName, player]) => {
+            // Check if player is playing a sound and if this sound has to be interrupted by the next one
+            if (!player.paused
+                && AudioPlayer.sounds[player.soundName].hasOwnProperty('interruptedBy')
+                && AudioPlayer.sounds[player.soundName].interruptedBy.includes(soundName)
+            ) {
+                // Will trigger player's 'pause' event
+                AudioPlayer.players[playerName].pause();
+            }
+        })
+
+        return AudioPlayer.initiatePlayer();
+    }
+
+    static killPlayers = () => {
+        Object.keys(AudioPlayer.players).forEach(playerName => AudioPlayer.players[playerName].pause());
+    }
 }
 
 export default AudioPlayer;
