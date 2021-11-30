f7afee4580ad856f67bb0260b0334aa6b0fc18fd
Emile Calixte <calixte.emile@gmail.com>
2021-11-30 22:29:43 +0100

    Call private API from backend to send contact form message

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,32 +2,30 @@ const MainController = require('./mainController/MainController');
 const EmailUtil = require("../util/EmailUtil");
 const InvalidEmailError = require("../errors/auth/email/InvalidEmailError");
 const EmptyBodyError = require("../errors/misc/EmptyBodyError");
+const FormData = require("form-data");
+const env = require("../config/env");
+const fetch = require("node-fetch");
 
 class UserController extends MainController {
 
     actionContact = async ({username, email, subject, message}) => {
-
         if (!EmailUtil.isEmailAddressValid(email)) throw new InvalidEmailError();
 
+        // TODO check username, subject & message max length
         if (subject === '' || message === '' || username === '') throw new EmptyBodyError();
 
-        await EmailUtil.sendEmailFromNoreply({
-            to: EmailUtil.CONTACT_ADDRESS,
-            subject: `${username} a envoyé un message  : ${subject}`,
-            replyTo: email,
-            html:
-`<p style="font-size: 1.4em; margin-bottom: 20px">${username} a envoyé un message : ${subject}</p>
-<p style="margin-bottom: 30px">${message}</p>
-<p>${username}</p>
-<p>${email}</p>
-`,
-            text:
-`${username} a envoyé un message : ${subject}.
-${message}
-${username}
-${email}
-`
-        })
+        const url = `${env.privateApiUrl}/receive-contact-form-message`;
+
+        const formData = new FormData();
+        formData.append('username', username);
+        formData.append('email', email);
+        formData.append('subject', subject);
+        formData.append('message', message);
+
+        await fetch (url, {
+            method: 'POST',
+            body: formData,
+        });
     }
 
     actionFeedback = async ({subject, message}) => {
