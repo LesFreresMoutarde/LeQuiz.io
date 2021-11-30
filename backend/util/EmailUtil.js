4f7905c357a590631f0268b00f9ccd5570fa9799
Emile Calixte <calixte.emile@gmail.com>
2021-11-30 21:50:15 +0100

    Refactored backend email sending functions in EmailUtil

--- backend/util/EmailUtil.js
+++ backend/util/EmailUtil.js
@@ -1,4 +1,6 @@
 const env = require('../config/env');
+const FormData = require("form-data");
+const fetch = require("node-fetch");
 
 class EmailUtil {
     /**
@@ -12,6 +14,33 @@ class EmailUtil {
 
         const regex = "^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
         return !!email.match(regex);
+    }
+
+    static sendResetPasswordEmail = async (email, username, resetPasswordUrl) => {
+        const url = `${env.privateApiUrl}/email/send-reset-password-email`;
+
+        const formData = new FormData();
+        formData.append('username', username);
+        formData.append('email', email);
+        formData.append('resetPasswordUrl', resetPasswordUrl);
+
+        await fetch(url, {
+            method: 'POST',
+            body: formData,
+        });
+    }
+
+    static sendWelcomeEmail = async (email, username) => {
+        const url = `${env.privateApiUrl}/email/send-welcome-email`;
+
+        const formData = new FormData();
+        formData.append('username', username);
+        formData.append('email', email);
+
+        await fetch(url, {
+            method: 'POST',
+            body: formData,
+        });
     }
 }
 
