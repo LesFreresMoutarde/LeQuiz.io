e07f2a0b4cf0450281a1d765c11029b79e038d1d
Emile Calixte <calixte.emile@gmail.com>
2021-11-30 21:20:03 +0100

    Remove nodemailer dependency & all useless functions in EmailUtil

--- backend/util/EmailUtil.js
+++ backend/util/EmailUtil.js
@@ -1,28 +1,6 @@
-const nodemailer = require('nodemailer');
 const env = require('../config/env');
 
 class EmailUtil {
-    static FROM_NAME = 'LeQuiz.io';
-    static FROM_NOREPLY_ADDRESS = 'noreply@lequiz.io';
-    static CONTACT_ADDRESS = 'contact@lequiz.io';
-
-    static transport = null; // Initialized by createTransport function
-
-    static createTransport = () => {
-        const transport = nodemailer.createTransport(env.email);
-
-        console.log('Verifying email transport');
-        transport.verify((error, success) => {
-            if (error) {
-                throw new Error(error);
-            } else {
-                console.log('Email transport is ready');
-            }
-        });
-
-        return transport;
-    }
-
     /**
      * @param {string} email
      * @return {boolean}
@@ -35,38 +13,6 @@ class EmailUtil {
         const regex = "^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
         return !!email.match(regex);
     }
-
-    /**
-     * @param {object} message The message options
-     * @param {string} message.from The sender
-     * @param {string} message.to The recipients, comma-separated
-     * @param {string} message.subject The email subject
-     * @param {string} message.text The email text content
-     * @param {string} message.html The email html content
-     * @param message
-     * @return {Promise}
-     * @throws {Error} if the email cannot be sent
-     */
-    static sendEmail = (message = {}) => {
-        return EmailUtil.transport.sendMail(message);
-    }
-
-    /**
-     *
-     * @param {object} message The message options
-     * @param {string} message.to The recipients, comma-separated
-     * @param {string} message.subject The email subject
-     * @param {string} message.text The email text content
-     * @param {string} message.html The email html content
-     * @returns {Promise}
-     */
-    static sendEmailFromNoreply = async (message = {}) => {
-        message.from = `"${EmailUtil.FROM_NAME}" <${EmailUtil.FROM_NOREPLY_ADDRESS}>`;
-
-        return await EmailUtil.sendEmail(message);
-    }
 }
-
-EmailUtil.transport = EmailUtil.createTransport();
 
 module.exports = EmailUtil;
