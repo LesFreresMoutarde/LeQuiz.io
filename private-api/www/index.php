d8415d53349860a060435acb3f19d039323d6f87
Emile Calixte <calixte.emile@gmail.com>
2021-12-09 18:36:05 +0100

    Handle case when error occurs before PrivateApi::static variable initialization

--- private-api/www/index.php
+++ private-api/www/index.php
@@ -7,6 +7,11 @@ require '../vendor/autoload.php';
 try { // TODO find a better way to catch and display all errors in dev mode ?
     (new PrivateApi())->run();
 } catch (Throwable $e) {
+
+    if (!isset(PrivateApi::$static)) {
+        die("<b>Error before initialization:</b> {$e->getMessage()}");
+    }
+
     if (PrivateApi::$static->getConfig()->isDevMode()) {
         dd($e);
     }
