786650cfe8818e852cfd3061b31466f0cfbcccd8
Emile Calixte <calixte.emile@gmail.com>
2025-05-20 20:04:18 +0200

    Remove contact & feedback buttons in footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,13 +6,13 @@ const Footer = ({displayFeedbackModal, mobile}) => {
     if (mobile) {
         return (
             <footer>
-                <div>
-                    <button className="footer-text-item footer-feedback-button" onClick={() => displayFeedbackModal()}>Feedback</button>
-                </div>
+                {/*<div>*/}
+                {/*    <button className="footer-text-item footer-feedback-button" onClick={() => displayFeedbackModal()}>Feedback</button>*/}
+                {/*</div>*/}
                 <div>
                     <span className="footer-text-item"><a href="/mentions-legales" className="no-underline" target="_blank">Mentions légales</a></span>
-                    -
-                    <span className="footer-text-item"><Link to="/contact" className="no-underline">Contact</Link></span>
+                    {/*-*/}
+                    {/*<span className="footer-text-item"><Link to="/contact" className="no-underline">Contact</Link></span>*/}
                 </div>
                 <div>
                     <span className="footer-text-item">© LeQuiz.io {(new Date()).getFullYear()}</span>
@@ -25,8 +25,8 @@ const Footer = ({displayFeedbackModal, mobile}) => {
         <footer>
             <span className="footer-text-item">© leQuiz.io {(new Date()).getFullYear()}</span>
             <span className="footer-text-item"><a href="/mentions-legales" className="no-underline" target="_blank">Mentions légales</a></span>
-            <span className="footer-text-item"><Link to="/contact" className="no-underline">Contact</Link></span>
-            <button className="footer-text-item footer-feedback-button" onClick={() => displayFeedbackModal()}>Feedback</button>
+            {/*<span className="footer-text-item"><Link to="/contact" className="no-underline">Contact</Link></span>*/}
+            {/*<button className="footer-text-item footer-feedback-button" onClick={() => displayFeedbackModal()}>Feedback</button>*/}
         </footer>
     );
 }
